ccda8b3c2ecacbe03f8c5cb574a3d68a42182feb
ZachPregl <zpregl1@gmail.com>
2023-10-06 23:31:51 +1000

    end of starting point

--- ontheway/LoginView.swift
+++ ontheway/LoginView.swift
@@ -7,47 +7,69 @@ struct LoginView: View {
     @Binding var pitstop: String
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Roundabout")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 20)
-            VStack(spacing: 20) {
-                TextField("Start", text: $start)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10)
-                TextField("Pitstop", text: $pitstop)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10)
-                    .overlay(
-                        Color.clear
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .modifier(WidthMultiplier(multiplier: 0.75))
-                    )
-
-                TextField("Finish", text: $finish)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10)
-                Button(action: { showFullMap.toggle() }) {
-                    Text("Route")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.accentColor)
-                        .cornerRadius(10)
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                Text("Roundabout")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 20)
+                
+                VStack(spacing: 20) {
+                    TextField("Start", text: $start)
+                        .textFieldStyle()
+                    
+                    TextField("Pitstop", text: $pitstop)
+                        .textFieldStyle()
+                        
+                    
+                    TextField("Finish", text: $finish)
+                        .textFieldStyle()
+                    
+                    Button(action: { showFullMap.toggle() }) {
+                        Text("Go Roundabout")
+                            .buttonStyle()
+                    }
+                    .frame(width: geometry.size.width - 40)
+                   
                 }
-            }
-            .padding(.horizontal, 20)
-            MapView()
-                .clipShape(RoundedRectangle(cornerRadius: 10))
                 .padding(.horizontal, 20)
-                .edgesIgnoringSafeArea(.bottom)
+                
+                MapView()
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal, 20)
+                    .edgesIgnoringSafeArea(.bottom)
+            }
         }
     }
 }
+
+extension View {
+    func textFieldStyle() -> some View {
+        self
+            .padding()
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10)
+    }
+    
+    func buttonStyle() -> some View {
+        self
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.accentColor)
+            .cornerRadius(10)
+    }
+}
+
+struct MapsView: View {
+    var body: some View {
+        MapView()
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(.horizontal, 20)
+                        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
