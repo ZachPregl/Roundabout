3016f38898718b32fec24127450399dc5a8def4d
ZachPregl <zpregl1@gmail.com>
2023-10-06 20:51:04 +1000

    Initial Commit

---
+++ ontheway/onthewayApp.swift
@@ -0,0 +1,17 @@
+//
+//  onthewayApp.swift
+//  ontheway
+//
+//  Created by Zach Pregl on 6/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct onthewayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
